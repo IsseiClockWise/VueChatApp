32315ef3d0563b9592097c96104f2bd6ed3b604c
IsseiClockWise <112615226+IsseiClockWise@users.noreply.github.com>
2023-06-28 18:06:57 +0900

    チャットルームの作成

--- frontend/src/components/ChatRooms.vue
+++ frontend/src/components/ChatRooms.vue
@@ -3,9 +3,14 @@
     <h1>VueChat - チャットルーム一覧</h1>
     <ul>
       <li v-for="room in chatRooms" :key="room.id">
-        <router-link :to="`http://localhost:3000/rooms/${room.id}`">{{ room.name }}</router-link>
+        {{ room.name }}
       </li>
     </ul>
+    <h3>チャットルーム作成</h3>
+    <input type="text" v-model="newRoomName" />
+    <div>
+      <button @click="createRoom">作成</button>
+    </div>
   </div>
 </template>
 
@@ -16,9 +21,11 @@ export default {
   data() {
     return {
       chatRooms: [],
+      newRoomName: '',
     };
   },
-  mounted() {
+  created() {
+    // コンポーネントが作成されたときに、チャットルーム一覧を取得する
     this.fetchChatRooms();
   },
   methods: {
@@ -27,6 +34,18 @@ export default {
         .get('http://localhost:3000/rooms')
         .then(response => {
           this.chatRooms = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    createRoom() {
+      axios.post('http://localhost:3000/rooms', {
+        name: this.newRoomName,
+      })
+        .then(response => {
+          this.chatRooms.push(response.data);
+          this.newRoomName = '';
         })
         .catch(error => {
           console.error(error);
